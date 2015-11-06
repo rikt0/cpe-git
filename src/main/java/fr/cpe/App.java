e4dd5ca6081f59f8d4a415300c2de07aa6bfdfe0
rikt0 <rick.trouss@gmail.com>
2015-11-06 11:19:53 +0100

    mon premier ommit

---
+++ src/main/java/fr/cpe/App.java
@@ -0,0 +1,14 @@
+package fr.cpe;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        //DateTimeNow = new DateTime();
+    }
+}
